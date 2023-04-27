1c9f3d0af1050314d4a1457d54d5e837ebbe7eaa
deepAnji281 <84462559+Deepaksaheb@users.noreply.github.com>
2023-04-27 21:21:20 +0530

    react-router setup done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react'
 import Navbar from './component/Navbar'
 import Form from './component/Form'
+import Alert from './component/Alert'
+import Contact from './component/Contact'
+import About from './component/About'
+import {Routes,Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className='App'>
-      <Navbar/>
+
+      <Routes>
+        <Route exact path='/' element={<Navbar/>} />
+        
+        <Route exact path ='/about' element={<About/>}/>
+        <Route  exact path='/contact' element={<Contact/>}/> 
+      </Routes>
       <Form/>
+      <Alert/>
     </div>
   )
 }
 
-export default App
+export default App;
---
+++ src/component/Alert.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Alert = (props) => {
+  return (
+    <div className='alert-massages'>
+      <div className="alert alert-warning alert-dismissible fade show" role="alert">
+  <strong> {props.Alert}</strong> 
+  <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+</div>
+  
+
+   
+    
+  )
+}
+
+export default Alert
